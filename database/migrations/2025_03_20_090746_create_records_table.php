9865cc635bddf9b8caf31d6ce06805afb5d57552
dicky hermawan <dhermawan215@gmail.com>
2025-03-20 16:21:10 +0700

    first

---
+++ database/migrations/2025_03_20_090746_create_records_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('device_id')->nullable();
+            $table->bigInteger('parameter_id')->nullable();
+            $table->double('value')->nullable();
+            $table->timestamp('recorded_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
